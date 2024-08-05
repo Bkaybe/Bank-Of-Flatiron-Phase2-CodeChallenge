b9b5219ef6c6a964716154c4e15ad27bf7c453f7
Bethuel Khisa <sunguribethuel@gmail.com>
2024-08-05 17:33:20 +0300

    Added the db.json data and modified it and also added the different components

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  // const urlfetch = "http://localhost:3000/transactions";
+
+  // fetch(urlfetch)
+  //   .then((res) => res.json())
+  //   .then((transactions) => console.log(transactions));
+  return (
+    <>
+      <header className="text-center bg-gray-700 text-3xl">BANK OF FLAT IRON</header>
+    </>
+  );
+}
+
+export default App;
