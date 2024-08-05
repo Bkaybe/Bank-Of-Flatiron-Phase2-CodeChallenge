e29132c327f722206cdbf1c2989d5fea34df639d
Bethuel Khisa <sunguribethuel@gmail.com>
2024-08-05 18:17:35 +0300

    Added a map method to iterate over the data and display a paragraph for each Data

--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
-
-const TransactionList = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+const TransactionList = ({ Transactions1 }) => {
+  return (
+    <div>
+      {Transactions1.map((transaction) => {
+        return (
+          <p key={transaction.id}>
+            Transaction Date: {transaction.date} , Description:
+            {transaction.descriprion}, Amount:
+            {transaction.amount}
+          </p>
+        );
+      })}
+    </div>
+  );
+};
 
 export default TransactionList;
