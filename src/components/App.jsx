498e5ddb8d008080563ae0bf970fad5ab38265e3
Bethuel Khisa <sunguribethuel@gmail.com>
2024-08-08 07:32:19 +0300

    Added search functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,25 +5,24 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 const App = () => {
+  const [transactions, setTransactions] = useState([]);
 
-  const [transactions, setTransactions] = useState([])
+  useEffect(() => {
+    axios.get(`http://localhost:3000/transactions`).then((res) => {
+      setTransactions(res.data);
+    });
+  }, []);
 
-  useEffect(()=> {
-    axios.get(`http://localhost:3000/transactions`)
-    .then((res) => {
-      setTransactions(res.data)
-    })
-  }, [])
+  console.log(transactions);
 
-  console.log(transactions)
   return (
     <>
       <header className="bg-[#282c34] flex flex-col items-center justify-center text-white text-[30px] pt-[10px] rounded-[15px]">
         BANK OF FLATIRON
       </header>
-      <SearchContainer />
+      <SearchContainer transactions={transactions} />
       <TransactionForm />
-      <TransactionList transactions={transactions}/>
+      <TransactionList transactions={transactions} />
     </>
   );
 };
