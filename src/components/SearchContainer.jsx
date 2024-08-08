66424a135462df142bf7f26027b6f3729c01bd5f
Bethuel Khisa <sunguribethuel@gmail.com>
2024-08-08 07:34:32 +0300

    Tried adding search functionality

--- src/components/SearchContainer.jsx
+++ src/components/SearchContainer.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import InputField from "./InputField";
 
-const SearchContainer = () => {
+const SearchContainer = ({transactions, setQuery}) => {
+
+  
   return (
     <div className="search-container">
       <form className="flex justify-center mt-[20px] items-centre">
-        <InputField placeholder="Search..." type="text" />
+        <InputField placeholder="Search..." type="text" onChange={(e) => setQuery(e.target.value)} />
       </form>
     </div>
   );
